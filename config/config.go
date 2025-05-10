b9466d41e09016e97bdc59e8557a5723d463adba
Mehmet EREN <mehmeter3n@gmail.com>
2025-05-10 10:54:30 +0300

    Add IP and Task-based rate limiting with configurable settings
    - Implemented two separate rate limiting middlewares: one for IP-based and one for Task-based limits.
    - Rate limit values are now configurable via environment variables.
    - Refactored configuration loading for better structure and readability.

--- config/config.go
+++ config/config.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -23,21 +24,43 @@ func LoadConfig() {
 		log.Fatalf("Error loading %s file: %v", envFile, err)
 	}
 
-	ExpiryMinute, err := strconv.Atoi(os.Getenv("JWT_EXPIRY_MINUTE"))
-	if err != nil {
-		log.Fatalf("Error Expiry Minute %s file: %v", envFile, err)
-	}
-
 	Cfg = Config{
 		AppEnv:     env,
 		ServerPort: os.Getenv("SERVER_PORT"),
 		Database: Database{
 			SQLDBUrl: os.Getenv("SQL_DATABASE_URL"),
 		},
-		JWT: JWTConfig{
-			Secret:       os.Getenv("JWT_SECRET"),
-			ExpiryMinute: ExpiryMinute,
-			RefreshTTL:   os.Getenv("REFRESH_TTL"),
-		},
+		JWT:           loadJWTConfig(),
+		RateLimit:     loadRateLimitConfig("RATE_LIMIT_MAX", "RATE_LIMIT_EXPIRATION"),
+		TaskRateLimit: loadRateLimitConfig("TASK_RATE_LIMIT_MAX", "TASK_RATE_LIMIT_EXPIRATION"),
 	}
+}
+
+func loadJWTConfig() JWTConfig {
+	ExpiryMinute, err := strconv.Atoi(os.Getenv("JWT_EXPIRY_MINUTE"))
+	if err != nil {
+		log.Fatalf("Error Expiry Minute %s", err)
+	}
+
+	return JWTConfig{
+		Secret:       os.Getenv("JWT_SECRET"),
+		ExpiryMinute: ExpiryMinute,
+		RefreshTTL:   os.Getenv("REFRESH_TTL"),
+	}
+}
+
+func loadRateLimitConfig(max_config_key, expiration_config_key string) RateLimitConfig {
+	maxStr := os.Getenv(max_config_key)
+	max, err := strconv.Atoi(maxStr)
+	if err != nil || max <= 0 {
+		max = 5
+	}
+
+	expStr := os.Getenv(expiration_config_key)
+	exp, err := time.ParseDuration(expStr)
+	if err != nil {
+		exp = time.Second
+	}
+
+	return RateLimitConfig{Max: max, Expiration: exp}
 }
